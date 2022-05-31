25aa76a9ed97887ba451d98e015197bbd4a9a16e
x260 <x260@DESKTOP-SDD09U8>
2022-05-31 17:11:18 +0700

    Add project files.

---
+++ training.netcorenvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace training.netcorenvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ training.netcorenvc/Models/TransactionDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace training.netcorenvc.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options) 
+        {}
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
